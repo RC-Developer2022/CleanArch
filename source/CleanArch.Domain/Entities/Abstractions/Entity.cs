fc63ce074aa2832092684c8a95a0dca9a352a586
RC-Developer2022 <rcoimbra2022@outlook.com>
2024-01-30 07:59:10 -0300

    init: Aprendendo CQRS com Mediat, Aulas do marcoratti

---
+++ source/CleanArch.Domain/Entities/Abstractions/Entity.cs
@@ -0,0 +1,11 @@
+﻿namespace CleanArch.Domain.Entities.Abstractions;
+
+public abstract class Entity
+{
+    public Guid Id { get; protected set; }
+
+    public Entity()
+    {
+        Id = Guid.NewGuid();
+    }
+}
---
+++ source/CleanArch.Domain/Entities/Member.cs
@@ -0,0 +1,23 @@
+﻿using CleanArch.Domain.Entities.Abstractions;
+using CleanArch.Domain.Validations;
+
+namespace CleanArch.Domain.Entities;
+
+public sealed class Member : Entity
+{
+    public string? FirstName { get; private set; }
+    public string? LastName { get; private set; }
+    public string? Gender { get; private set; }
+    public string? Email { get; private set; }
+    public bool? IsActive { get; private set; }
+
+    public Member(string firstName, string lastName, string gender, string email, bool isActive)
+    {
+        ValidateDomain(firstName, lastName, gender, email, isActive);
+    }
+
+    private void ValidateDomain(string firstName, string lastName, string gender, string email, bool isActive) 
+    {
+        DomainValidation.When(string.IsNullOrEmpty(firstName), "Invalid name. FirstName is required!");
+d    }
+}
